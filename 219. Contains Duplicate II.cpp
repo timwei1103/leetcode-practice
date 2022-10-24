aaa56ad163c23225ae229b5be29af44cbd0ef989
timwei1103 <87876012+timwei1103@users.noreply.github.com>
2022-10-24 21:54:40 +0800

    Update 219. Contains Duplicate II.cpp

--- 219. Contains Duplicate II.cpp
+++ 219. Contains Duplicate II.cpp
@@ -11,3 +11,18 @@ public:
         return false;
     }
 };
+
+//hash_set方式
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(s.count(nums[i]))return true;
+            s.insert(nums[i]);
+            //只往前找k個 超過就刪掉
+            if(s.size()>k)s.erase(nums[i-k]);
+        }
+        return false;
+    }
+};
